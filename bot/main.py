40754ec46753e413c94bf0d48d8dd72a89cfda1d
Lisa <e.gryumova@yandex.ru>
2024-04-15 17:58:14 +0300

    add table init

--- bot/main.py
+++ bot/main.py
@@ -10,21 +10,26 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from handlers.wallet_handlers import router_wallet
 from handlers.main_handler import router_main
 from db.tasks import update_balances, update_wallets
+from db.db import Database
 
 load_dotenv()
 # TOKEN = os.getenv('BOT_TOKEN')
 TOKEN = "7020669369:AAEZZVBUfTN6h6CkeTjsDUPg3lK4BPzXVlI"
 
+db = Database()
+
 async def main():
+    db.initaialTables()
+    
     bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
     dp = Dispatcher(storage=MemoryStorage())
 
     dp.include_router(router_wallet)
     dp.include_router(router_main)
 
-    # loop = asyncio.get_event_loop()
-    # loop.create_task(update_balances(bot))
-    # loop.create_task(update_wallets(bot))
+    loop = asyncio.get_event_loop()
+    loop.create_task(update_balances(bot))
+    loop.create_task(update_wallets(bot))
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
